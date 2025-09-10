d9f4ff507db9fd74ae4908cc3488325da56f548d
endrasakya076-pixel <endrasakya076@gmail.com>
2025-09-11 05:55:24 +0800

    coomit laravel2

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    return "Hallo Hendra";
+    return "Hallo Hendra Sakya Permana";
 });
 
 Route::get('/hello', function () {
