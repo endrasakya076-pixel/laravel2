99c09ea7e27e9613c62443bd3bd9c17d9de5a99f
endrasakya076-pixel <endrasakya076@gmail.com>
2025-09-14 20:53:52 +0800

    coomit laravel2

--- routes/web.php
+++ routes/web.php
@@ -4,42 +4,42 @@ use App\Http\Controllers\HaloController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    //return view('welcome');
-    return "Hallo Hendra Sakya Permana";
-});
+// Route::get('/', function () {
+//     //return view('welcome');
+//     return "Hallo Hendra Sakya Permana";
+// });
 
-Route::get('/hello', function () {
-    return "Saya akan belajar dengan sungguh-sungguh";
-});
+// Route::get('/hello', function () {
+//     return "Saya akan belajar dengan sungguh-sungguh";
+// });
 
-Route::get('/bisa', function () {
-    return "Mencoba Menjadi yang lebih baika";
-});
+// Route::get('/bisa', function () {
+//     return "Mencoba Menjadi yang lebih baika";
+// });
 
-Route::get('/selamat', function () {
-    return view('welcome');
-});
+// Route::get('/selamat', function () {
+//     return view('welcome');
+// });
 
-Route::get('/yakin', function () {
-    return "Saya akan terus mencoba hingga bisa menjadi mahir";
-});
+// Route::get('/yakin', function () {
+//     return "Saya akan terus mencoba hingga bisa menjadi mahir";
+// });
 
-Route::get('/halo', function () {
-    return view('halo');
-});
+// Route::get('/halo', function () {
+//     return view('halo');
+// });
 
-Route::get('/page', function () {
-    return view('page');
-});
+// Route::get('/page', function () {
+//     return view('page');
+// });
 
-Route::get('/hallo', [HaloController::class, 'index']);
+// Route::get('/hallo', [HaloController::class, 'index']);
 
-Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
-Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
-Route::get('/buku/{id}/edit', [BukuController::class, 'edit'])->name('buku.edit');
-Route::put('/buku/{id}/update', [BukuController::class, 'update'])->name('buku.update');
-Route::delete('/buku/{id}/delete', [BukuController::class, 'destroy'])->name('buku.delete');
+// Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
+// Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
+// Route::get('/buku/{id}/edit', [BukuController::class, 'edit'])->name('buku.edit');
+// Route::put('/buku/{id}/update', [BukuController::class, 'update'])->name('buku.update');
+// Route::delete('/buku/{id}/delete', [BukuController::class, 'destroy'])->name('buku.delete');
 
 
 Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard.index');
