401b36fd5b2dd049a97b2e494da1567735ca4ef8
endrasakya076-pixel <endrasakya076@gmail.com>
2025-09-14 21:32:32 +0800

    coomit laravel2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -67,8 +67,27 @@
             <!-- Nav Item - Tables -->
             <li class="nav-item">
                 <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+                    <i class="fas fa-tasks"></i>
+                    <span>Data tugas</span></a>
+            </li>
+<!-- Heading -->
+            <div class="sidebar-heading">
+                MENU KARYAWAN
+            </div>
+
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="charts.html">
+                    <i class="fas fa-user"></i>
+                    <span>Data user</span></a>
+            </li>
+
+            <!-- Nav Item - Tables -->
+            <li class="nav-item">
+                <a class="nav-link" href="tables.html">
+                    <i class="fas fa-tasks"></i>
+                    <span>Data tugas</span></a>
             </li>
 
             <!-- Divider -->
