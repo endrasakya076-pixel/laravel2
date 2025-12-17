8bebf264839c4198136f755689204b441c3fb582
endrasakya076-pixel <endrasakya076@gmail.com>
2025-12-17 22:39:58 +0800

    coomit laravel2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts/app')
 
 @section('content')
-<h1 class="h3 mb-4 text-gray-800">{{ $title }}</h1>
+<h1 class="h3 mb-4 text-gray-800">
+    <i class="fas fa-fw fa-tachometer-alt"></i>
+    {{ $title }}</h1>
 @endsection
  
