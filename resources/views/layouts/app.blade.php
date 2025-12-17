c58fe3a428b22fac3a807c6cb5a8977ab5f0f109
endrasakya076-pixel <endrasakya076@gmail.com>
2025-12-17 22:21:10 +0800

    coomit laravel2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Manajemen Tugas</title>
+    <title>M Tugas | {{ $title }}</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('sbadmn2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
