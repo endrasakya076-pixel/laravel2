97d4549960c0af497754d7d6dd1c71a497897edf
endrasakya076-pixel <endrasakya076@gmail.com>
2025-12-17 22:27:02 +0800

    coomet laravel2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,9 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $data = array(
+            "title" => "Dashboard"
+        );
+        return view('dashboard', $data);
     }
 }
