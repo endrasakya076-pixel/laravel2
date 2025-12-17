a777da1632eae9fb05b1d32793fb214c76cb4d94
endrasakya076-pixel <endrasakya076@gmail.com>
2025-12-17 22:54:12 +0800

    coomit laravel2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item aktive">
+            <li class="nav-item active">
                 <a class="nav-link" href="index.html">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
