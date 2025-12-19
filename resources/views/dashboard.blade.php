3903a6bc402cbfda4b1fce3c674531f13bb63d1e
endrasakya076-pixel <endrasakya076@gmail.com>
2025-12-19 09:58:27 +0800

    coomit laravel2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,26 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts/app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+@section('content')
+<h1 class="h3 mb-4 text-gray-800">
+    <i class="fas fa-fw fa-tachometer-alt mr-2"></i>
+    {{ $title }}</h1>
+    <div class="row">
+         <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Total User</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-user fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
     </div>
-</x-app-layout>
+@endsection
+ 
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,45 @@
 <?php
-
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\HaloController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     //return view('welcome');
+//     return "Hallo Hendra Sakya Permana";
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/hello', function () {
+//     return "Saya akan belajar dengan sungguh-sungguh";
+// });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::get('/bisa', function () {
+//     return "Mencoba Menjadi yang lebih baika";
+// });
 
-require __DIR__.'/auth.php';
+// Route::get('/selamat', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/yakin', function () {
+//     return "Saya akan terus mencoba hingga bisa menjadi mahir";
+// });
+
+// Route::get('/halo', function () {
+//     return view('halo');
+// });
+
+// Route::get('/page', function () {
+//     return view('page');
+// });
+
+// Route::get('/hallo', [HaloController::class, 'index']);
+
+// Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
+// Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
+// Route::get('/buku/{id}/edit', [BukuController::class, 'edit'])->name('buku.edit');
+// Route::put('/buku/{id}/update', [BukuController::class, 'update'])->name('buku.update');
+// Route::delete('/buku/{id}/delete', [BukuController::class, 'destroy'])->name('buku.delete');
+
+
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard.index');
