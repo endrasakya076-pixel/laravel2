d65b8c4db4a3ccbd04de8dacfb3fd076e3c13c40
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-08 17:00:06 +0800

    coomit laravel2

--- resources/views/admin/tugas/spesimen.blade.php
+++ resources/views/admin/tugas/spesimen.blade.php
@@ -37,42 +37,17 @@
                 @enderror
             </div>
         </div>
-        <div class="row mb-4">
-            <div class="col-xl-12">
-                <label class="form-label">
-                   <span class="text-danger">*</span>
-                    Jabatan :</label>
-                <select name="jabatan" class="form-control @error('jabatan') is-invalid @enderror">
-                    <option selected disabled>--Pilih Jabatan--</option>
-                    <option value="Admin">Admin</option>
-                    <option value="Karyawan">Karyawan</option>
-                </select>
-                @error('jabatan')
-                <small class="text-danger">
-                    {{ $message }}   
-                </small>
-                @enderror
-            </div>            
-        </div>
-        <div class="row mb-4">
-            <div class="c ol-xl-6 mb-2">
+        <div class="col-xl-6 mb-1">
                 <label class="form-label">
                     <span class="text-danger">*</span>
-                    Password :</label>
-                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror">
-                @error('password')
+                    Nama:</label>
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}">
+                    @error('email')
                 <small class="text-danger">
                     {{ $message }}   
                 </small>
                 @enderror
             </div>
-            <div class="col-xl-6">
-                <label class="form-label">
-                    <span class="text-danger">*</span>
-                    Password Konfirmasi :</label>
-                <input type="password" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror">
-            </div>
-        </div>
         <div>
             <button type="submit" class="btn btn-sm btn-primary">
                 <i class="fas fa-save mr-2"></i>
