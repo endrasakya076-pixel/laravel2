b0dd10ed7a054a021adf7be7d625d3ad334e2ea7
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-09 10:35:55 +0800

    coomit laravel2

--- resources/views/admin/tugas/index.blade.php
+++ resources/views/admin/tugas/index.blade.php
@@ -52,7 +52,7 @@
                                         </tr>
                                     </thead>
                                    <tbody>
-                                    @foreach ($spesimen as $item)
+                                    @foreach 
                                         <tr>
                                             {{-- <td class="text-center">{{ $loop->iteration }}</td> --}}
                                             <td><img src="{{ $item->foto }}" alt="Foto" width="50"></td>
