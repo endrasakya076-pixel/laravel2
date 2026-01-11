a19163525bd135cffc7de6301a1c58c5b57d6a17
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-11 17:25:09 +0800

    coomit laravel2

--- resources/views/admin/tugas/index.blade.php
+++ resources/views/admin/tugas/index.blade.php
@@ -59,7 +59,7 @@
 
                                             {{-- 2. Menampilkan foto (asumsi folder ada di public/storage) --}}
                                             <td>
-                                            {{-- <a href="{{ asset('images/'. $item->foto) }}" target="_blank" rell="noopener noreferrer">Lihat Spesimen</a> --}}
+                                            <a href="{{ asset('images/'. $item->foto) }}" target="_blank" rell="noopener noreferrer">.</a>
                                             <img src="{{ asset('images/'. $item->foto) }}" width="30%" height="30%" alt="" srcset="">
                                             </td>
                                             {{-- 3. Menampilkan data teks dari kolom database --}}
