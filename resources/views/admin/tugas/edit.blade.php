123404c3ea4b29c0e6039ecb992e36b1b23a1770
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-11 20:14:10 +0800

    coomit laravel2

--- resources/views/admin/tugas/edit.blade.php
+++ resources/views/admin/tugas/edit.blade.php
@@ -14,7 +14,7 @@
         <form action="{{ route('tugasStore') }}" method="post" enctype="multipart/form-data">
             @csrf
     <div class="row mb-4">
-    <div class="col-xl-6 mb-8">
+    <div class="col-xl-6">
         <label class="form-label">
             <span class="text-danger">*</span>
             Foto Spesimen :
