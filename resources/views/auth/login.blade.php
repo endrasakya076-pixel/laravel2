872e78fbfa1cd9b18ba762b9d82099d26071e767
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-12 11:16:57 +0800

    coomit laravel2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,12 +62,12 @@
                                                     </small>
                                                 @enderror
                                         </div>
-                                        {{-- <div class="form-group">
+                                        <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                {{-- <input type="checkbox" class="custom-control-input" id="customCheck">
                                                 <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div> --}}
+                                                    Me</label> --}}
+                                            </div>
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Login
