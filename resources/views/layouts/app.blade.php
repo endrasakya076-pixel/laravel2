ac5fd2ea376ff0b0d0f7c8b34de50699999216b8
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-12 11:16:15 +0800

    coomit laravel2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
+                        <span>Copyright &copy; PT. BPR Prima Nadi 2026</span>
                     </div>
                 </div>
             </footer>
