8d4f42edfce450a18dd582f033207c3638c48274
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-12 09:18:05 +0800

    coomit laravel2

--- resources/views/admin/tugas/index.blade.php
+++ resources/views/admin/tugas/index.blade.php
@@ -23,7 +23,7 @@
         </div> --}}
                     <!-- Search Form -->
                      <form
-                       action="{{ route('tugasSearch') }}" method="GET">
+                       action="{{ url('tugasSearch') }}" method="GET">
                         <div class="input-group">
                             <input name="search" type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
                                 aria-label="Search" aria-describedby="basic-addon2">
