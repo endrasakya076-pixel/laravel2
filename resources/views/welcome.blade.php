81a1bfd8f8d941db68b45340c69e09997e63bbe0
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-12 10:47:13 +0800

    coomit laravel2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,12 +45,12 @@
       <a href="#" class="logo d-flex align-items-center me-auto">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">eNno</h1>
+        <h1 class="sitename">PT. BPR PRIMA NADI</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="#hero" class="active">Home</a></li>
+          {{-- <li><a href="#hero" class="active">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
@@ -71,7 +71,7 @@
               <li><a href="#">Dropdown 3</a></li>
               <li><a href="#">Dropdown 4</a></li>
             </ul>
-          </li>
+          </li> --}}
           <li><a href="#contact">Contact</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
