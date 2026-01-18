516d7d55d8c8c5266a6f249e149801e55a2d1af9
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-18 16:10:41 +0800

    coomit laravel2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,22 +6,7 @@
     {{ $title }}</h1>
     <div class="row">
          <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                User</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-user fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-{{-- @if(auth()->id() == 1)
+                            @if(auth()->id() == 1)
 <div class="card mt-4">
     <div class="card-header bg-dark text-white">
         <h5 class="mb-0">Log Aktivitas Admin (Terbaru)</h5>
@@ -51,7 +36,9 @@
         </table>
     </div>
 </div>
-@endif --}}
+@endif
+                        </div>
+
     </div>
 @endsection
  
