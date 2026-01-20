c35d324f06de4235d30c7c3bef5286e9e8bfcf85
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-20 15:18:44 +0800

    coomit laravel2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
     <div class="container">
       <div class="header-content">
         <a href="#" class="logo">
-          <img src="{{ asset('images/logo/logoprima.png') }}" class="logo-image" style="height: 30px; width: 40px;">
+          <img src="{{ asset('images/logo/logoprima.png') }}" class="logo-image" style="height: 30px; width: 60px;">
         </a>
       </div>
     </div>
