8b9bf3825b3933a9a3d45fb6efed4048e00a1c1d
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-22 09:09:12 +0800

    coomit laravel2

--- config/app.php
+++ config/app.php
@@ -123,16 +123,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-    'providers' => [
-        /*
-         * Package Service Providers...
-         */
-        Barryvdh\DomPDF\ServiceProvider::class,
-    ],
-    
-    'aliases' => [
-        'PDF' => Barryvdh\DomPDF\Facade\Pdf::class,
-    ],
-
 ];
