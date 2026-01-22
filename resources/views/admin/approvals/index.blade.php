2275106966952940a734a9c2f787b62076d60178
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-22 23:31:51 +0800

    coomit laravel2

--- resources/views/admin/approvals/index.blade.php
+++ resources/views/admin/approvals/index.blade.php
@@ -32,4 +32,25 @@
         </tbody>
     </table>
 </div>
+
+@if(session('success'))
+<div class="modal fade show" id="successModal" tabindex="-1" role="dialog" aria-hidden="true" style="display: block; background: rgba(0, 0, 0, 0.5);">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-success"><i class="fas fa-check-circle"></i> Sukses</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>{{ session('success') }}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
 @endsection
