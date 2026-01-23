bd0f4fc7c9651931b614702d4d72e6dd851a0577
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-23 11:21:46 +0800

    coomit laravel2

--- resources/views/admin/tugas/index.blade.php
+++ resources/views/admin/tugas/index.blade.php
@@ -195,6 +195,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 function kembaliKeSpesimen() {
     // Arahkan kembali ke halaman daftar tugas spesimen
-    window.location.href = "{{ route('tugasSpesimen') }}";
+    window.location.href = "{{ route('tugas.index') }}";
 }
 </script>
