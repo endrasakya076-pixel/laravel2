228524b1598eb345bcde3729e0702d8d1b915246
endrasakya076-pixel <endrasakya076@gmail.com>
2026-01-23 15:18:22 +0800

    coomit laravel2

--- resources/views/admin/approvals/index.blade.php
+++ resources/views/admin/approvals/index.blade.php
@@ -39,11 +39,11 @@
 
     <td class="text-center">
         @if($approval->status == 'Hapus')
-            <span class="badge badge-success"><i class="fas fa-check-circle"></i> Hapus</span>
+            <span class="badge badge-danger"><i class="fas fa-check-circle"></i> Hapus</span>
         @elseif($approval->status == 'Ditolak')
             <span class="badge badge-danger"><i class="fas fa-times-circle"></i> Ditolak</span>
         @elseif($approval->status == 'Setuju')
-            <span class="badge badge-warning text-dark"><i class="fas fa-pause-circle"></i> Setuju</span>
+            <span class="badge badge-success"><i class="fas fa-pause-circle"></i> Setuju</span>
         @else
             <span class="badge badge-secondary">Baru Masuk</span>
         @endif
